56fedafd42fdf537e65f7d266c9e98cb08763e8b
Lucas <lucasmruiz05@gmail.com>
2023-06-22 16:30:13 -0300

    ADd filter and fix updatee

--- DB/index.py
+++ DB/index.py
@@ -1,1 +1,16 @@
-## EN ESTA CARPETA VAMOS A INICIALIZAR LA DB.
+import psycopg2
+
+def initialize_database():
+    try:
+        connection = psycopg2.connect(
+            host='localhost',
+            port='5432',
+            user='postgres',
+            password='postgres',
+            database='UTN-FRSR-Kodifikades',
+        )
+        print('Conexión exitosa!')
+        cursor = connection.cursor()
+        return cursor
+    except Exception as ex:
+        print(ex)
