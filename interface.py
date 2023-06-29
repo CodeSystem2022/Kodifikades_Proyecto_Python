984f2f11e1c7eaa587ca760ad44efc565c741bef
Miguel Angel Bru Melis <brumelis545@gmail.com>
2023-06-29 15:52:24 -0300

    funcionalidad visualizador columnas

--- interface.py
+++ interface.py
@@ -127,3 +127,25 @@ class Interface:
         # Buscar
         self.buscarBtn = Button(self.raiz, text="Buscar", bd=2, font=('Arial', 20), bg="#ff8000",width=15, command=self.buscar)
         self.buscarBtn.grid(row=7, column=6, padx=(30, 0), pady=(0, 40))
+        
+        #--------------------------
+        #-------VISUALIZADOR-------
+        # -------------------------
+
+        # Tabla
+        self.tabla_datos = ttk.Treeview(self.raiz)
+        self.style = ttk.Style()
+        self.style.configure("Treeview.Heading", font=('Arial Bold', 15))
+        self.tabla_datos['columns'] = ("Id","Nombre","Apellido","DNI","Email","Telefono","Edad","Estado","Fecha Incorporacion","Departamento","Salario")
+        self.tabla_datos.column("#0", width=0, stretch=NO)
+        self.tabla_datos.column("Id", anchor=W, width=150)
+        self.tabla_datos.column("Nombre", anchor=W, width=150)
+        self.tabla_datos.column("Apellido", anchor=W, width=150)
+        self.tabla_datos.column("DNI", anchor=W, width=150)
+        self.tabla_datos.column("Email", anchor=W, width=230)
+        self.tabla_datos.column("Telefono", anchor=W, width=150)
+        self.tabla_datos.column("Edad", anchor=W, width=150)
+        self.tabla_datos.column("Estado", anchor=W, width=150)
+        self.tabla_datos.column("Fecha Incorporacion", anchor=W, width=150)
+        self.tabla_datos.column("Departamento", anchor=W, width=150)
+        self.tabla_datos.column("Salario", anchor=W, width=150)
